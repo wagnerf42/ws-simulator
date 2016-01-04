bb4a10291d5e73b37ff8cf4adc1a5d90dc3716a5
mkhatiri <khatiri.med@gmail.com>
2016-01-04 00:51:48 +0000

    update handle file

--- Simulation/handle_file.pl
+++ Simulation/handle_file.pl
@@ -1,10 +1,11 @@
-package Handle_tree;
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
 my $results_file = $ARGV[0];
-die "please give following args : $results_file" unless -f $results_file ;
+my $processors_number = $ARGV[1];
+die "please give following args : results_file processors number" unless -f $results_file and $processors_number=~/^\d+$/;
 
 
 trait_file($results_file);
@@ -16,14 +17,16 @@ sub trait_file{
 	chomp($line);		
 	my @split_line = split(/ /,$line);	
 	print $split_line[0]." ; ".$split_line[1]." ; ".$split_line[2]." ; ".$split_line[3]." ; W/D ; D-W ; ";	
-	print $split_line[4]."; ".$split_line[5]." ; T_attendu / T_simul ; T_simul - T_attendu \n";	
+	print $split_line[4]."; ".$split_line[5]." ; T_Sequentiel ; T_simul - T_attendu  ; T_Simul - T_Sequentiel \n";	
 	while($line = <FILE>) {
 	chomp($line);		        
 	@split_line = split(/ /,$line);
+	
+	my $t_sequentiel = ($split_line[0]**3)/$processors_number ;
 	print $split_line[0]." ; ".$split_line[1]." ; ".$split_line[2].";".$split_line[3];
 	print "; ". ($split_line[2]/$split_line[3]) ." ; ". ($split_line[3]-$split_line[2]) ." ; "  ;
-	print $split_line[4]." ; ".$split_line[5];
-	print " ; ". ($split_line[4]/$split_line[5]) ." ; ". ($split_line[5]-$split_line[4] )." \n"  ;
+	print $split_line[4]." ; ".$split_line[5] ." ; ".$t_sequentiel ;
+	print " ; ". ($split_line[5] - $split_line[4]) ." ; ". ($split_line[5] - $t_sequentiel)." \n"  ;
 	}
 	return;
 }
